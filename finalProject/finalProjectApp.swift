c76db56cbe79ea8204efc923208c2f0059696bf4
rachel-156 <rltimes10@gmail.com>
2023-07-14 10:40:10 -0400

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Rachel Liang on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
