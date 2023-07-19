8a85ed4e8401675e9767c2835ff51647eeba59a4
Raaina <raaina.daniar@gmail.com>
2023-07-19 14:51:26 -0400

    Added Zstack to profile and background

--- finalProject/profileView.swift
+++ finalProject/profileView.swift
@@ -14,29 +14,32 @@ struct profileView: View {
     
     var body: some View {
         NavigationStack {
-            VStack{
-                TextField(" Username", text: $username)
-                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                    .padding(.horizontal)
-                    .font(.title)
-                
-                TextField(" Password", text: $password)
-                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                    .padding(.horizontal)
-                    .font(.title)
-                
-                NavigationLink(destination: ContentView()) {
-                    Text("Login")
+            ZStack{
+                Color(red: 241/255, green: 239/255, blue: 231/255)
+                    .ignoresSafeArea()
+                VStack{
+                    TextField(" Username", text: $username)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                        .padding(.horizontal)
+                        .font(.title)
+                    
+                    TextField(" Password", text: $password)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                        .padding(.horizontal)
+                        .font(.title)
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("Login")
+                    }
+                    .padding(.trailing)
+                    .font(.title2)
                 }
-                .padding(.trailing)
-                .font(.title2)
+                
             }
             
         }
-        
     }
 }
-
 struct profileView_Previews: PreviewProvider {
     static var previews: some View {
         profileView()
