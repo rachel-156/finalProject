72e522ccbe9be747ef8b5350959fb09c4b88efcb
Raaina <raaina.daniar@gmail.com>
2023-07-20 21:04:48 -0400

    Fixed timer and it is now at the bottom

--- finalProject/studyTrackerView.swift
+++ finalProject/studyTrackerView.swift
@@ -11,63 +11,69 @@ struct studyTrackerView: View {
     @StateObject private var vm = ViewModel()
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     private let width: Double = 250
-        @State var reservationDate = Date()
-
-            var body: some View {
-                Form {
-                    HStack {
-                        DatePicker(
-                            selection: $reservationDate, in: Date()...,
-                            displayedComponents: [.date, .hourAndMinute]
-                        ) {}
-                        Button(action: { print("do something!") }) {
-                            HStack {
-                                Image(systemName: "arrow.right.circle")
-                                Text("Done")
-                            }
-                        }.padding(20)
-                    }
-                    Text("Date is \(reservationDate.formatted(date: .long, time: .complete))")
-                        .padding()
-                    VStack {
-                        Text("\(vm.time)")
-                            .font(.system(size: 70, weight: .medium, design: .rounded))
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .frame(width: width)
-                            .background(.thinMaterial)
-                            .cornerRadius(20)
-                            .overlay(RoundedRectangle(cornerRadius: 20)
-                                .stroke(Color.gray, lineWidth: 4))
-                            .alert("Timer Done", isPresented: $vm.showingAlert) {
-                                Button("Continue", role: .cancel) {
-                                    //code
-                                }
-                            }
-                        Slider(value: $vm.minutes, in: 1...25, step: 1)
-                            .padding()
-                            .frame(width: width)
-                            .disabled(vm.isActive)
-                            .animation(.easeInOut, value: vm.minutes)
-                        
-                        HStack(spacing: 50) {
-                            Button("Start") {
-                                vm.start(minutes: vm.minutes)
-                            }
-                            .disabled(vm.isActive)
-                            
-                            Button("Reset", action: vm.reset)
-                                .tint(.red)
-                            
+    @State var reservationDate = Date()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                HStack {
+                    DatePicker(
+                        selection: $reservationDate, in: Date()...,
+                        displayedComponents: [.date, .hourAndMinute]
+                    ) {}
+                    Button(action: { print("do something!") }) {
+                        HStack {
+                            Image(systemName: "arrow.right.circle")
+                            Text("Done")
                         }
-                        .frame(width: width)
-                    }
-                    .onReceive(timer) { _ in
-                        vm.updateCountdown()
-                    }
+                    }.padding(20)
                 }
+                Text("Date is \(reservationDate.formatted(date: .long, time: .complete))")
+                    .padding()
+            }
+            VStack {
+                Text("\(vm.time)")
+                    .font(.system(size: 70, weight: .medium, design: .rounded))
+                    .alert("Timer done!", isPresented: $vm.showingAlert) {
+                        Button("Continue", role: .cancel) {
+                            // Code
+                        }
+                    }
+                    .padding()
+                    .frame(width: width)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                
+                Slider(value: $vm.minutes, in: 1...25, step: 1)
+                    .padding()
+                    .disabled(vm.isActive)
+                    .animation(.easeInOut, value: vm.minutes)
+                    .frame(width: width)
+
+                HStack(spacing:50) {
+                    Button("Start") {
+                        vm.start(minutes: vm.minutes)
+                    }
+                    .disabled(vm.isActive)
+                    
+                    Button("Reset", action: vm.reset)
+                        .tint(.red)
+                }
+                .frame(width: width)
+            }
+            .onReceive(timer) { _ in
+                vm.updateCountdown()
+            }
+
             }
         }
+    }
+
+        
     
 
 
